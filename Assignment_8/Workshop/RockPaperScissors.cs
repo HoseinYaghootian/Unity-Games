a9f54c8447c5c4b1491168c9974fc28454515e90
HoseinYaghootian <hyone2084@gmail.com>
2022-04-06 22:28:48 +0430

    Add files via upload

---
+++ Assignment_8/Workshop/RockPaperScissors.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Workspace
+{
+    class RockPaperScissors
+    {
+        static void Main(string[] args)
+        {
+            int userScore = 0, computerScore = 0;
+            int roundNumber = 5;
+
+            for (int i = 0; i < roundNumber; i++)
+            {
+                string[] options = { "Rock", "Paper", "Scissor" };
+
+                Random r = new Random();
+                int ccn = r.Next(0, 3);
+                string computerChoice = options[ccn];
+
+                Console.WriteLine("Round " + (i + 1) + "\r\nPlease Write Number of Your Choice:\r\n1-Rock 2-Paper 3-Scissor:");
+                int ucn = Convert.ToInt32(Console.ReadLine());
+                string userChoice = options[ucn - 1];
+
+                if (userChoice == "Rock" && computerChoice == "Scissor" || userChoice == "Paper" && computerChoice == "Rock" || userChoice == "Scissor" && computerChoice == "Paper")
+                {
+                    userScore++;
+                }
+                else if (userChoice == computerChoice)
+                {
+                    /*Nothing*/
+                }
+                else
+                {
+                    computerScore++;
+                }
+
+                Console.WriteLine("User Choice:     " + userChoice);
+                Console.WriteLine("Computer Choice: " + computerChoice);
+                Console.WriteLine("User Score:      " + userScore);
+                Console.WriteLine("Computer Score:  " + computerScore);
+                Console.WriteLine("-----------------------------");
+
+                //Early Win!
+                if (roundNumber - i < userScore || roundNumber - i < computerScore)
+                {
+                    break;
+                }
+            }
+
+            if (userScore > computerScore)
+            {
+                Console.WriteLine("User Win");
+            }
+            else if (userScore < computerScore)
+            {
+                Console.WriteLine("Computer Win");
+            }
+            else
+            {
+                Console.WriteLine("Draw");
+            }
+        }
+    }
+}
+
---
+++ Assignment_8/Workshop_1/PalamPoloomPlush.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Workspace_1
+{
+    class PalamPoloomPlush
+    {
+        static void Main(string[] args)
+        {
+            int userScore = 0, computer1Score = 0, computer2Score = 0;
+            int i = 0;
+
+            while (userScore != 5 && computer1Score != 5 && computer2Score != 5)
+            {
+                string[] options = { "Ro", "Posht" };
+
+                Random r1 = new Random();
+                int cc1n = r1.Next(0, 2);
+                string computer1Choice = options[cc1n];
+
+                Random r2 = new Random();
+                int cc2n = r2.Next(0, 2);
+                string computer2Choice = options[cc2n];
+
+                Console.WriteLine("Round " + (i + 1) + "\r\nPlease Write Number of Your Choice:\r\n1-Ro 2-Posht:");
+                int ucn = Convert.ToInt32(Console.ReadLine()) - 1;
+                string userChoice = options[ucn];
+
+                if (ucn != cc1n && cc1n == cc2n)
+                {
+                    userScore++;
+                }
+                else if (cc1n != ucn && ucn == cc2n)
+                {
+                    computer1Score++;
+                }
+                else if (cc2n != ucn && ucn == cc1n)
+                {
+                    computer2Score++;
+                }
+                else
+                {
+                    /*Nothing*/
+                }
+
+                i++;
+
+                Console.WriteLine("User Choice:       " + userChoice);
+                Console.WriteLine("Computer 1 Choice: " + computer1Choice);
+                Console.WriteLine("Computer 2 Choice: " + computer2Choice);
+
+                Console.WriteLine("User Score:        " + userScore);
+                Console.WriteLine("Computer 1 Score:  " + computer1Score);
+                Console.WriteLine("Computer 2 Score:  " + computer2Score);
+                Console.WriteLine("-----------------------------");
+            }
+
+            if (userScore == 5)
+            {
+                Console.WriteLine("User Win");
+            }
+            else if (computer1Score == 5)
+            {
+                Console.WriteLine("Computer 1 Win");
+            }
+            else
+            {
+                Console.WriteLine("Computer 2 Win");
+            }
+        }
+    }
+}
+
